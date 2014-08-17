2c7be4deaf049a7a624dc46473a6f164d2a7dee6
delius <pawel@fibrotrust.org>
2014-08-17 13:41:27 +0100

    defined campaign setup

--- app/controllers/setup_campaign_controller.rb
+++ app/controllers/setup_campaign_controller.rb
@@ -1,4 +1,13 @@
 class SetupCampaignController < ApplicationController
 include Wicked::Wizard
 steps :campaign_setup
+
+def show
+	@user = current_user
+	case steps
+	when :campaign_setup
+	@campaign = Campaign.new
+end
+render_wizard
+
 end
--- app/views/devise/registrations-controller.rb
+++ app/views/devise/registrations-controller.rb
@@ -1,5 +1,5 @@
 
 def after_sign_up_path_for(resource)
 	session[ :plan_id] = params [ :plan_id]
-	setup_organization_path( :organization_setup)
+	setup_campaign_path( :campaign_setup)
 end
